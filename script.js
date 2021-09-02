5c3e28655bbfb389f56a8342c3f09494d9b5b22c
Matthew Jiang <matthew.m.jiang@gmail.com>
2021-09-01 18:49:48 -0700

    added interval functionality

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let timeDisplay = document.getElementById("time-display");
+let intervalsDisplay = document.getElementById("intervals-display");
+let loop;
 let pomo =  {"time": 25 * 60,
             "intervals": 3,
+            "mode": "work",
+            "numintervals": 3,
             "work": 25 * 60,
             "shortbreak": 5 * 60,
-            "longbreak": 20 * 60};
+            "longbreak": 15 * 60};
 let startButton = document.getElementById("start-button");
 
+/* Timer logic */
 function runTimer() {
     if (pomo.time == 0) {
-        clearInterval();
-        interval--;
+        clearInterval(loop);
+        updatePomo();
+        console.log("timer stopped");
     }
     else {
         pomo.time--;
@@ -17,6 +23,7 @@ function runTimer() {
     }
 }
 
+/* Convert time (stored in seconds) to HH:MM:SS */
 function secondsToString() {
     let hours = Math.floor(pomo.time / 3600);
     let minutes = Math.floor(pomo.time % 3600 / 60);
@@ -28,13 +35,34 @@ function secondsToString() {
 }
 
 function updatePomo() {
-    if 
+    
+    if (pomo.mode == "work") {  // just finished an interval
+        pomo.intervals--;
+        if (pomo.intervals == 0) {  // go to longbreak if we're done with our intervals
+            pomo.mode = "longbreak";
+            pomo.intervals = pomo.numintervals;  // reset intervals
+        }
+        else {  // otherwise go to shortbreak
+            pomo.mode = "shortbreak";
+        } 
+        intervalsDisplay.textContent = pomo.intervals;  // update intervals display
+    }
+
+    else {  // otherwise go to work
+        pomo.mode = "work";
+    }
+    pomo.time = pomo[pomo.mode];  // reset timer to appropriate time
+}
+
+function resetTime() {
+    
 }
 
 startButton.addEventListener('click', () => {
     pomo.time++;  // add 1 second so we can see the full length time when the button is pressed
+    intervalsDisplay.textContent = pomo.intervals;
     runTimer();  // manual call so it doesn't take a second to see the time
-    setInterval(runTimer, 1000);
+    loop = setInterval(runTimer, 1);
 })
 
 
